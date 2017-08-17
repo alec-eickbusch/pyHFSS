7976048254162ecfc38ab3840c42c63daa9b8bc8
Alec Eickbusch <alec.eickbusch@yale.edu>
2017-08-17 15:51:50 -0400

    more bbq tests

--- Scripts/Alec/11ghz/bbq_test.py
+++ Scripts/Alec/11ghz/bbq_test.py
@@ -30,4 +30,25 @@ sol            = bba.sols
 meta_datas     = bba.meta_data
 hfss_variables = bba.hfss_variables
 
- 
+### Testing with things from Zlatko's code
+reload(bbq)
+from bbq  import BbqAnalysis
+bba = BbqAnalysis(bbp.data_filename)
+print "Available variations: ", bba.variations
+
+cos_trunc = 10;   fock_trunc  = 7;
+CHI_O1, CHI_ND, PJ, Om, EJ, diff, LJs, SIGN, f0s, f1s, fzpfs, Qs, varz = \
+    bba.analyze_variation(variation = '0', cos_trunc = cos_trunc,   fock_trunc  = fock_trunc)
+s         = sol[variation];   
+meta_data = meta_datas[variation]
+varz      = hfss_variables[variation]    
+CHI_O1, CHI_ND, PJ, Om, EJ, diff, LJs, SIGN, f0s, f1s, fzpfs, Qs = \
+    eBBQ_Pmj_to_H_params(s, meta_data, cos_trunc = cos_trunc, fock_trunc = fock_trunc)
+
+print '\nPJ=\t(renorm.)';        print_matrix(PJ*SIGN, frmt = "{:7.4f}")
+#print '\nCHI_O1=\t PT. [alpha diag]'; print_matrix(CHI_O1,append_row ="MHz" )
+print '\nf0={:6.2f} {:7.2f} {:7.2f} GHz'.format(*f0s)
+print '\nCHI_ND=\t PJ O(%d) [alpha diag]'%(cos_trunc); print_matrix(CHI_ND, append_row ="MHz")
+print '\nf1={:6.2f} {:7.2f} {:7.2f} GHz'.format(*(f1s*1E-9))   
+print 'Q={:8.1e} {:7.1e} {:6.0f}'.format(*(Qs))
+print pd.Series({ key:varz[key] for key in ['_join_w','_join_h','_padV_width', '_padV_height','_padH_width', '_padH_height','_scaleV','_scaleH', '_LJ1','_LJ2'] })
